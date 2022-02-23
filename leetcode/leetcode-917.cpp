96b7d208e65244d075e7b63ce2a3ed5f8aa26744
Simcc <192670992@qq.com>
2022-02-23 10:49:34 +0800

    每日两题

--- leetcode/leetcode-917.cpp
+++ leetcode/leetcode-917.cpp
@@ -6,9 +6,16 @@ using namespace std;
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        int l = 0, r = s.size()-1;
-        while(l < r){
-            while(l < r && !((s[l] >= 'a' && s[l] <='z')||(s[l] >= 'A' && s[l] <='Z'))) l++;
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            while (l < r && !((s[l] >= 'a' && s[l] <= 'z') || (s[l] >= 'A' && s[l] <= 'Z'))) l++;
+            while (l < r && !((s[r] >= 'a' && s[r] <= 'z') || (s[r] >= 'A' && s[r] <= 'Z'))) r--;
+            if (l < r) {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
         }
+        return s;
     }
 };
